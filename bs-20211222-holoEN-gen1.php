7c408d8de4287f9ac71b74838e51c691d92cc158
ruiru <pepper102756@gmail.com>
2022-02-24 20:37:37 +0800

    Myth&Conucil update

--- bs-20211222-holoEN-gen1.php
+++ bs-20211222-holoEN-gen1.php
@@ -1,9 +1,54 @@
 <?php
 echo '[
-{"id" : "001" ,"img" : "Gawr_Gura.png" ,"height":"141cm","birthday":"6/20","design":"甘城なつき","title" : "Gawr_Gura", "herf" : "bs-20211222-holoEN-About-Gura.html", "youtube" : "https://www.youtube.com/channel/UCoSrY_IQQVpmIRZ9Xf-y93g"},
-{"id" : "002" ,"img" : "ina3.jpg" ,"height":"157cm","birthday":"5/20","design":"黑星紅白","title" : "Ninomae Ina nis", "herf" : "bs-20211222-holoEN-About-Ina.html", "youtube" : "https://www.youtube.com/channel/UCMwGHR0BTZuLsmjY_NT5Pwg"},
-{"id" : "003" ,"img" : "unnamed.jpg" ,"height":"150cm","birthday":"1/6","design":"なび","title" : "Watson Amelia", "herf" : "bs-20211222-holoEN-About-Ame.html", "youtube" : "https://www.youtube.com/channel/UCyl1z3jo3XHR1riLFKG5UAg"},
-{"id" : "004" ,"img" : "cali.jpg" ,"height":"167cm","birthday":"4/4","design":"ゆきさめ","title" : "Mori Calliope", "herf" : "bs-20211222-holoEN-About-Calli.html", "youtube" : "https://www.youtube.com/channel/UCL_qhgtOy0dy1Agp8vkySQg"},
-{"id" : "005" ,"img" : "kiara-1.jpg" ,"height":"165cm","birthday":"7/6","design":"huke","title" : "Takanashi Kiara", "herf" : "bs-20211222-holoEN-About-Kiara.html", "youtube" : "https://www.youtube.com/channel/UCHsx4Hqa-1ORjQTh9TYDhww"}
+{
+	"id" : "001" ,
+	"img" : "Gawr_Gura.png" ,
+	"height":"141cm",
+	"birthday":"06/20",
+	"design":"甘城なつき",
+	"title" : "Gawr_Gura", 
+	"herf" : "bs-20211222-holoEN-About-Gura.html", 
+	"youtube" : "https://www.youtube.com/channel/UCoSrY_IQQVpmIRZ9Xf-y93g"
+},
+{
+	"id" : "002" ,
+	"img" : "ina3.jpg" ,
+	"height":"157cm",
+	"birthday":"05/20",
+	"design":"黑星紅白",
+	"title" : "Ninomae Ina nis", 
+	"herf" : "bs-20211222-holoEN-About-Ina.html", 
+	"youtube" : "https://www.youtube.com/channel/UCMwGHR0BTZuLsmjY_NT5Pwg"
+},
+{
+	"id" : "003" ,
+	"img" : "unnamed.jpg" ,
+	"height":"150cm",
+	"birthday":"01/06",
+	"design":"なび",
+	"title" : "Watson Amelia", 
+	"herf" : "bs-20211222-holoEN-About-Ame.html", 
+	"youtube" : "https://www.youtube.com/channel/UCyl1z3jo3XHR1riLFKG5UAg"
+},
+{
+	"id" : "004" ,
+	"img" : "cali.jpg" ,
+	"height":"167cm",
+	"birthday":"04/04",
+	"design":"ゆきさめ",
+	"title" : "Mori Calliope", 
+	"herf" : "bs-20211222-holoEN-About-Calli.html", 
+	"youtube" : "https://www.youtube.com/channel/UCL_qhgtOy0dy1Agp8vkySQg"
+},
+{
+	"id" : "005" ,
+	"img" : "kiara-1.jpg" ,
+	"height":"165cm",
+	"birthday":"07/06",
+	"design":"huke",
+	"title" : "Takanashi Kiara", 
+	"herf" : "bs-20211222-holoEN-About-Kiara.html", 
+	"youtube" : "https://www.youtube.com/channel/UCHsx4Hqa-1ORjQTh9TYDhww"
+}
 ]'
 ?>
